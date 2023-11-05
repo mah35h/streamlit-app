09944d2568d574bfd0cd5f9da4ccb1be037a0fdd
Mahesh Urkude <mah35h@users.noreply.github.com>
2023-11-05 09:44:39 +0530

    14

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import streamlit as st
 import openai
 
 # Replace 'YOUR_API_KEY' with your actual OpenAI API key
-api_key = 'YOUR_API_KEY'
+api_key = openai-key
 
 # Function to generate questions using GPT-3.5
 def generate_questions(text, max_tokens=50):
